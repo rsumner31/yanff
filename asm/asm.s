940b10328286f77451ff6aeeab07c160174bca81
Robert Sumner Jr <rsumner31@gmail.com>
2018-02-15 01:57:12 -0600

    Initial commit
    Conflicts:
    	.gitignore
    	CONTRIBUTING.md
    	Makefile
    	README.md
    	asm/asm.go
    	asm/asm.s
    	common/common.go
    	examples/Dockerfile
    	examples/Firewall.go
    	examples/Forwarding.go
    	examples/Makefile
    	examples/demo.go
    	examples/dump.go
    	flow/flow.go
    	low/low.go
    	mk/include.mk
    	mk/intermediate.mk
    	mk/leaf.mk
    	packet/packet.go
    	packet/packet_test.go
    	scheduler/scheduler.go
    	test/Makefile
    	test/config.go
    	test/dockerlauncher.go
    	test/logging.go
    	test/main/.gitignore
    	test/main/Makefile
    	test/main/config.json
    	test/main/main.go
    	test/main/perf.json
    	test/main/stability.json
    	test/performance/.gitignore
    	test/performance/Dockerfile
    	test/performance/Makefile
    	test/performance/perf_light.go
    	test/performance/perf_main.go
    	test/performance/perf_seq.go
    	test/report.go
    	test/report_test.go
    	test/stability/Makefile
    	test/stability/test1/Dockerfile
    	test/stability/test1/Makefile
    	test/stability/test1/common/test1_common.go
    	test/stability/test1/part1.go
    	test/stability/test1/part2.go
    	test/stability/test_merge/Dockerfile
    	test/stability/test_merge/Makefile
    	test/stability/test_merge/test-merge-part1.go
    	test/stability/test_merge/test-merge-part2.go
    	test/stability/test_partition/Dockerfile
    	test/stability/test_partition/Makefile
    	test/stability/test_partition/test-partition-part1.go
    	test/stability/test_partition/test-partition-part2.go
    	test/stability/test_separate/Dockerfile
    	test/stability/test_separate/Makefile
    	test/stability/test_separate/test-separate-l3rules.conf
    	test/stability/test_separate/test-separate-part1.go
    	test/stability/test_separate/test-separate-part2.go
    	test/stability/test_split/Dockerfile
    	test/stability/test_split/Makefile
    	test/stability/test_split/test-split-part1.go
    	test/stability/test_split/test-split-part2.go
    	test/stability/test_split/test-split.conf
    	test/stash/.gitignore
    	test/stash/Dockerfile
    	test/stash/Makefile
    	test/stash/create_packet_example.go
    	test/stash/forwardingTestL3.go
    	test/testsuite.go
    	test/types.go

--- asm/asm.s
+++ asm/asm.s
@@ -36,7 +36,63 @@ TETEXT ·Rte_compiler_rmb(SB),NOSPLIT,$0-0
 TEXT ·Rte_compiler_wmb(SB),NOSPLIT,$0-0
 >>>>>>>-14e22e5
     SFENCE
+EXT ·RteTEXT ·Rte_compiler_rmb(SB),NOSPLIT,$0-0
+        LFENCE
         RET
+TEXT ·Rte_compiler_wmb(SB),NOSPLIT,$0-0
+        SFENCE
+>>>>>>>-14e22e5
+
+TEXT ·Prefetcht0(SB),NOSPLIT,$0-8
+        MOVQ    addr+0(FP), AX
+        PREFETCHT0      (AX)
+        RET
+// Copyright 2017 Intel Corporation. 
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+<<<<<<<+HEAD
+TEXT ·RteCompilerRmb(SB),NOSPLIT,$0-0
+        LFENCE
+        RET
+TEXT ·RteCompilerWmb(SB),NOSPLIT,$0-0
+=====
+TETEXT ·Rte_compiler_rmb(SB),NOSPLIT,$0-0
+        LFENCE
+        RET
+TEXT ·Rte_compiler_wmb(SB),NOSPLIT,$0-0
+>>>>>>>-14e22e5
+    SFENCE
+        RET
+TEXT ·Prefetcht0(SB),NOSPLIT,$0-8
+        MOVQ    addr+0(FP), AX
+        PREFETCHT0      (AX)
+        RET
+// Copyright 2017 Intel Corporation. 
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "textflag.h"
+<<<<<<<+HEAD
+TEXT ·RteCompilerRmb(SB),NOSPLIT,$0-0
+        LFENCE
+        RET
+TEXT ·RteCompilerWmb(SB),NOSPLIT,$0-0
+=====
+TETEXT ·Rte_compiler_rmb(SB),NOSPLIT,$0-0
+        LFENCE
+        RET
+TEXT ·Rte_compiler_wmb(SB),NOSPLIT,$0-0
+>>>>>>>-14e22e5
+    SFENCE
+EXT ·RteTEXT ·Rte_compiler_rmb(SB),NOSPLIT,$0-0
+        LFENCE
+        RET
+TEXT ·Rte_compiler_wmb(SB),NOSPLIT,$0-0
+        SFENCE
+>>>>>>>-14e22e5
+
 TEXT ·Prefetcht0(SB),NOSPLIT,$0-8
         MOVQ    addr+0(FP), AX
         PREFETCHT0      (AX)
